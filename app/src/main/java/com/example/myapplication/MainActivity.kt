711303d08584b3d7bc2a68e1e1de24c5d3810261
KJunser <kmin07250725@gmail.com>
2021-08-18 22:16:29 +0900

    login and register3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.image.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mButton.setOnClickListener({
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
+
+
         })
+        login_btn.setOnClickListener({
+            val intent = Intent(this, image::class.java)
+            startActivity(intent)
+        })
+
     }
+
 }
+
