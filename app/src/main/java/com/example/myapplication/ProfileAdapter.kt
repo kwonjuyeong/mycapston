18c92716dc67fc59f36a554cfffdf8a57c2245a2
KJunser <kmin07250725@gmail.com>
2021-08-25 20:28:08 +0900

    login and register 0.5

--- app/src/main/java/com/example/myapplication/ProfileAdapter.kt
+++ app/src/main/java/com/example/myapplication/ProfileAdapter.kt
@@ -17,11 +17,14 @@ class ProfileAdapter(val profileList: ArrayList<Profiles> ) : RecyclerView.Adapt
     }
 
     override fun onBindViewHolder(holder: ProfileAdapter.CustomViewHolder, position: Int) {
-
+    holder.gender.setImageResource(profileList.get(position).gender)
+        holder.names.text = profileList.get(position).names
+        holder.age.text = profileList.get(position).age.toString()
+        holder.job.text = profileList.get(position).job
     }
 
     override fun getItemCount(): Int {
-    return profileList
+    return profileList.size
     }
 
 
@@ -29,7 +32,7 @@ class ProfileAdapter(val profileList: ArrayList<Profiles> ) : RecyclerView.Adapt
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val gender = itemView.findViewById<ImageView>(R.id.iv_profile) //가져올 이미지
-        val name = itemView.findViewById<TextView>(R.id.tv_name) //이름
+        val names = itemView.findViewById<TextView>(R.id.tv_name) //이름
         val age = itemView.findViewById<TextView>(R.id.tv_name) //이름
         val job = itemView.findViewById<TextView>(R.id.tv_name) //이름
 
