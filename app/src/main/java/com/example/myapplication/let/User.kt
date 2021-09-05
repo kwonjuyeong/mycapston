a60f1c05e5c2384f84c8f6e072566b6589091a16
kwonjuyoung <20161527@vision.hoseo.edu>
2021-09-05 23:04:58 +0900

    그대로 추가함 수정해야됨.

---
+++ app/src/main/java/com/example/myapplication/let/User.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.let
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("business_name")
+    val businessName: String,
+    @SerializedName("business_number")
+    val businessNumber: String,
+    @SerializedName("officer_email")
+    val officerEmail: String,
+    @SerializedName("officer_name")
+    val officerName: String,
+    @SerializedName("officer_phone")
+    val officerPhone: String,
+    @SerializedName("officer_position")
+    val officerPosition: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("industry")
+    val industry: String,
+    @SerializedName("location_name")
+    val locationName: String
+)
---
+++ app/src/main/java/com/example/myapplication/let/UserInfoRepository.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.let
+//package com.mtjin.envmate.data.user_info
+
+import com.example.myapplication.let.User
+import com.example.myapplication.let.SignUpRes
+import io.reactivex.Single
+
+interface UserInfoRepository {
+    fun insertUserInfo(user: User): Single<SignUpRes>
+    fun requestSignUpAccept(email: String): Single<SignUpRes>
+}
